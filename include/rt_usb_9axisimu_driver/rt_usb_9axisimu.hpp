e45850e9dbcc91d2bc71a17ae2b62819956d980f
Shun Hasegawa <shop@rt-net.jp>
2015-09-26 14:38:05 +0900

    Changes to be committed: 	new file:   CMakeLists.txt 	modified:   LICENSE 	new file:   include/rt_usb_9axisimu_driver/rt_usb_9axisimu.hpp 	new file:   launch/rt_usb_9axisimu_driver.launch 	new file:   package.xml 	new file:   src/rt_usb_9axisimu_driver.cpp

---
+++ include/rt_usb_9axisimu_driver/rt_usb_9axisimu.hpp
@@ -0,0 +1,252 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 RT Corporation
+// All rights reserved.
+
+// License: BSD
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// * Neither the name of RT Corporation nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//------------------------------------------------------------------------------
+#include <math.h>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sstream>
+
+/**********************************************************************************************************
+ *
+ * Constants
+ *
+ **********************************************************************************************************/
+
+namespace RtUsbImu 
+{       
+  class Consts 
+  {
+    public:
+      //IMU Data table constants
+      enum{
+        IMU_TIMESTAMP = 7,
+        IMU_ACC_X_L = 8,
+        IMU_ACC_X_H = 9,
+        IMU_ACC_Y_L = 10,
+        IMU_ACC_Y_H = 11,
+        IMU_ACC_Z_L = 12,
+        IMU_ACC_Z_H = 13,
+        IMU_TEMP_L = 14,
+        IMU_TEMP_H = 15,
+        IMU_GYRO_X_L = 16, 
+        IMU_GYRO_X_H = 17,
+        IMU_GYRO_Y_L = 18,
+        IMU_GYRO_Y_H = 19,
+        IMU_GYRO_Z_L = 20,
+        IMU_GYRO_Z_H = 21,
+        IMU_MAG_X_L = 22,
+        IMU_MAG_X_H = 23,
+        IMU_MAG_Y_L = 24,
+        IMU_MAG_Y_H = 25,
+        IMU_MAG_Z_L = 26,
+        IMU_MAG_Z_H = 27,
+        IMU_DATA_SIZE = 28,
+      };
+      //Convertor
+      const double CONVERTOR_RAW2G;
+      const double CONVERTOR_RAW2DPS;
+      const double CONVERTOR_RAW2UT;
+      const double CONVERTOR_RAW2C_1;
+      const double CONVERTOR_RAW2C_2;
+      const double CONVERTOR_G2A;    
+      const double CONVERTOR_D2R;
+      const double CONVERTOR_UT2T;
+      const double DEFAULT_LINEAR_ACCELERATION_STDDEV;
+      const double DEFAULT_ANGULAR_VELOCITY_STDDEV;
+      const double DEFAULT_MAGNETIC_FIELD_STDDEV;
+
+      Consts()
+      : CONVERTOR_RAW2G(2048)      // for linear_acceleration (raw data to [g])
+      , CONVERTOR_RAW2DPS(16.4)    // for angular_velocity (raw data to [degree/s])
+      , CONVERTOR_RAW2UT(0.3)      // for magnetic_field (raw data to [uT])
+      , CONVERTOR_RAW2C_1(340)     // for temperature (raw data to celsius)
+      , CONVERTOR_RAW2C_2(35)      // for temperature (raw data to celsius)
+      , CONVERTOR_G2A(9.80665)     // for linear_acceleration (g to m/s^2)
+      , CONVERTOR_D2R(M_PI/180.0)  // for angular_velocity (degree to radian)
+      , CONVERTOR_UT2T(1000000)    // for magnetic_field (uT to Tesla)
+      , DEFAULT_LINEAR_ACCELERATION_STDDEV(0.023145)    // Default of square root of the linear_acceleration_covariance diagonal elements in m/s^2. 
+      , DEFAULT_ANGULAR_VELOCITY_STDDEV(0.0010621)      // Default of square root of the angular_velocity_covariance diagonal elements in rad/s.
+      , DEFAULT_MAGNETIC_FIELD_STDDEV(0.00000080786)    // Default of square root of the magnetic_field_covariance diagonal elements in Tesla.
+      {}
+
+      ~Consts(){
+      }
+  };
+
+/**********************************************************************************************************
+ *
+ * Serial port abstraction
+ *
+ **********************************************************************************************************/
+
+  class SerialPort
+  {
+    private:
+      std::string port_name; // ex) "/dev/ttyACM0"
+      int port_fd;
+
+    public:
+      SerialPort(const char* port = "") : port_name(port), port_fd(-1){
+      }
+      
+      ~SerialPort() {
+        Close();
+      }
+
+      bool Open(const char* port) {
+        port_name = port;
+        return Open();
+      }
+
+      bool Open() {
+        int fd = 0;
+
+        if(port_fd > 0) {
+          return true;
+        }
+
+        fd = open(port_name.c_str(), O_RDWR | O_NOCTTY);  //Open serial port
+        if(fd < 0) {
+          return false;  //Port open error
+        }
+
+        port_fd = fd;
+
+        return (fd > 0);
+
+      }
+
+      void Close() {
+        if(port_fd > 0) {
+          close(port_fd);  //Close serial port
+          port_fd = -1;
+        }
+      }
+
+      int Read(unsigned char* buf, unsigned int buf_len) {
+        if(port_fd < 0) {
+          return -1;
+        }
+
+        return read(port_fd, buf, buf_len);  //Read device file and put the data into buf (max size is (buf_len) bytes)
+                                             //Process stops here until data comes
+      }
+
+      int Write(unsigned char* data, unsigned int data_len) {
+        if(port_fd < 0) {
+          return -1;
+        }
+
+        return write(port_fd, data, data_len);
+      }
+  };
+  
+  /**************************************************************************
+   *
+   * IMU
+   *
+   **************************************************************************/
+
+  //Class to store either raw integer imu data or converted physical quantity
+  template <typename Type> class ImuData 
+  {
+    public:
+      int timestamp;
+      Type ax, ay, az, gx, gy, gz, mx, my, mz, temperature;
+
+      ImuData() {
+        Reset();
+      }
+      
+      ~ImuData(){
+      }
+
+      inline void Reset() {
+        timestamp = -1;
+        ax = ay = az = gx = gy = gz = mx = my = mz = temperature = 0;
+      }
+  };
+
+  class SensorData
+  {
+    private:
+      ImuData<signed short> imu_rawdata;
+      ImuData<double> imu;
+      Consts consts;
+
+    public:
+      SensorData() {
+        Reset();
+      }
+
+      ~SensorData(){
+      }
+
+      void Reset() {
+        imu_rawdata.Reset();
+        imu.Reset();
+      }
+
+      void update_imu_raw(ImuData<signed short>& i) {
+        imu_rawdata = i;
+      }
+
+      //Method to convert raw integer imu data to physical quantity
+      void convert_rawdata() {  
+        imu.timestamp = imu_rawdata.timestamp;
+
+        // Convert raw data to [g]
+        imu.ax = imu_rawdata.ax/consts.CONVERTOR_RAW2G;
+        imu.ay = imu_rawdata.ay/consts.CONVERTOR_RAW2G;
+        imu.az = imu_rawdata.az/consts.CONVERTOR_RAW2G;
+
+        // Convert raw data to [degree/s]
+        imu.gx = imu_rawdata.gx/consts.CONVERTOR_RAW2DPS;
+        imu.gy = imu_rawdata.gy/consts.CONVERTOR_RAW2DPS;
+        imu.gz = imu_rawdata.gz/consts.CONVERTOR_RAW2DPS;
+
+        // Convert raw data to [uT]
+        imu.mx = imu_rawdata.mx*consts.CONVERTOR_RAW2UT;
+        imu.my = imu_rawdata.my*consts.CONVERTOR_RAW2UT;
+        imu.mz = imu_rawdata.mz*consts.CONVERTOR_RAW2UT;
+
+        // Convert raw data to celsius
+        imu.temperature = imu_rawdata.temperature/consts.CONVERTOR_RAW2C_1 + consts.CONVERTOR_RAW2C_2;
+      }
+      
+      ImuData<double> output_imu_data() {
+        return imu;
+      }
+  };
+};
---
+++ src/rt_usb_9axisimu_driver.cpp
@@ -0,0 +1,217 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2015 RT Corporation
+// All rights reserved.
+
+// License: BSD
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+
+// * Neither the name of RT Corporation nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//------------------------------------------------------------------------------
+#include "rt_usb_9axisimu_driver/rt_usb_9axisimu.hpp"
+
+#include "ros/ros.h"
+
+#include "sensor_msgs/Imu.h"
+#include "sensor_msgs/MagneticField.h"
+#include "std_msgs/Float64.h"
+
+#include <sstream>
+
+using namespace RtUsbImu;
+
+class RtUsb9axisimuDriverForROS : public SerialPort
+{
+  private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_priv_;
+
+    ros::Publisher imu_data_raw_pub_;
+    ros::Publisher imu_mag_pub_;
+    ros::Publisher imu_temperature_pub_;
+
+    SensorData sensor_data_;
+
+    std::string frame_id_;
+    double linear_acceleration_stddev_;
+    double angular_velocity_stddev_;
+    double magnetic_field_stddev_;
+    Consts consts;
+
+  public:
+    RtUsb9axisimuDriverForROS(std::string port="") : SerialPort(port.c_str()), nh_priv_("~")
+    {
+      // dependent on user device
+      nh_priv_.setParam("port", port);
+      // default frame id
+      nh_priv_.param("frame_id", frame_id_, std::string("imu_link"));
+      // defaults obtained experimentally from device
+      nh_priv_.param("linear_acceleration_stddev", linear_acceleration_stddev_, consts.DEFAULT_LINEAR_ACCELERATION_STDDEV);
+      nh_priv_.param("angular_velocity_stddev", angular_velocity_stddev_, consts.DEFAULT_LINEAR_ACCELERATION_STDDEV);
+      nh_priv_.param("magnetic_field_stddev", magnetic_field_stddev_, consts.DEFAULT_LINEAR_ACCELERATION_STDDEV);
+      // publisher for streaming
+      imu_data_raw_pub_   = nh_.advertise<sensor_msgs::Imu>("imu/data_raw", 1);
+      imu_mag_pub_        = nh_.advertise<sensor_msgs::MagneticField>("imu/mag", 1);
+      imu_temperature_pub_= nh_.advertise<std_msgs::Float64>("imu/temperature", 1);
+    }
+
+    ~RtUsb9axisimuDriverForROS(){
+    }
+
+    bool Start(){
+      if(Open() == false)
+        return false;
+      return true;
+    }
+
+    //Method to combine two separate one-byte data into one two-byte data 
+    signed short CombineByteData(unsigned char data_h, unsigned char data_l){
+      signed short short_data = 0;
+
+      short_data = data_h;
+      short_data = short_data << 8;
+      short_data |= data_l;
+
+      return short_data;
+    }
+
+    //Method to extract sensor data from communication buffer 
+    ImuData<signed short> ExtractSensorData(unsigned char* imu_data_buf){
+      ImuData<signed short> imu_rawdata; 
+
+      imu_rawdata.timestamp = imu_data_buf[consts.IMU_TIMESTAMP];
+      imu_rawdata.temperature = CombineByteData(imu_data_buf[consts.IMU_TEMP_H], imu_data_buf[consts.IMU_TEMP_L]);
+      imu_rawdata.ax = CombineByteData(imu_data_buf[consts.IMU_ACC_X_H], imu_data_buf[consts.IMU_ACC_X_L]);
+      imu_rawdata.ay = CombineByteData(imu_data_buf[consts.IMU_ACC_Y_H], imu_data_buf[consts.IMU_ACC_Y_L]);
+      imu_rawdata.az = CombineByteData(imu_data_buf[consts.IMU_ACC_Z_H], imu_data_buf[consts.IMU_ACC_Z_L]);
+      imu_rawdata.gx = CombineByteData(imu_data_buf[consts.IMU_GYRO_X_H], imu_data_buf[consts.IMU_GYRO_X_L]);
+      imu_rawdata.gy = CombineByteData(imu_data_buf[consts.IMU_GYRO_Y_H], imu_data_buf[consts.IMU_GYRO_Y_L]);
+      imu_rawdata.gz = CombineByteData(imu_data_buf[consts.IMU_GYRO_Z_H], imu_data_buf[consts.IMU_GYRO_Z_L]);
+      imu_rawdata.mx = CombineByteData(imu_data_buf[consts.IMU_MAG_X_H], imu_data_buf[consts.IMU_MAG_X_L]);
+      imu_rawdata.my = CombineByteData(imu_data_buf[consts.IMU_MAG_Y_H], imu_data_buf[consts.IMU_MAG_Y_L]);
+      imu_rawdata.mz = CombineByteData(imu_data_buf[consts.IMU_MAG_Z_H], imu_data_buf[consts.IMU_MAG_Z_L]);
+
+      return imu_rawdata;
+    }
+
+    //Method to receive IMU data, convert those units to SI, and publish to ROS topic 
+    bool GetAndPublishData(){
+      unsigned char imu_data_buf[consts.IMU_DATA_SIZE];
+      ImuData<signed short> imu_rawdata; 
+      ImuData<double> imu; 
+      while(ros::ok()){
+
+        if(Read(imu_data_buf, consts.IMU_DATA_SIZE) != consts.IMU_DATA_SIZE)  //Wait until data comes
+          return false;  //Communication error
+
+        imu_rawdata = ExtractSensorData(imu_data_buf);  //Extract sensor data
+        sensor_data_.update_imu_raw(imu_rawdata);       //Update raw data
+        sensor_data_.convert_rawdata();                 //Convert raw data to physical quantity
+        imu = sensor_data_.output_imu_data();           //Get phisical quantity
+
+        sensor_msgs::Imu imu_data_raw_msg;
+        sensor_msgs::MagneticField imu_magnetic_msg;
+        std_msgs::Float64 imu_temperature_msg;
+
+        //Calculate linear_acceleration_covariance diagonal elements
+        double linear_acceleration_cov = linear_acceleration_stddev_ * linear_acceleration_stddev_;
+        //Calculate angular_velocity_covariance diagonal elements
+        double angular_velocity_cov    = angular_velocity_stddev_ * angular_velocity_stddev_;
+        //Calculate magnetic_field_covariance diagonal elements
+        double magnetic_field_cov      = magnetic_field_stddev_ * magnetic_field_stddev_;
+
+        //imu_data_raw_msg has no orientation values
+        imu_data_raw_msg.orientation_covariance[0] = -1;
+
+        imu_data_raw_msg.linear_acceleration_covariance[0] =
+          imu_data_raw_msg.linear_acceleration_covariance[4] =
+          imu_data_raw_msg.linear_acceleration_covariance[8] = linear_acceleration_cov;
+
+        imu_data_raw_msg.angular_velocity_covariance[0] =
+          imu_data_raw_msg.angular_velocity_covariance[4] =
+          imu_data_raw_msg.angular_velocity_covariance[8] = angular_velocity_cov;
+
+        imu_magnetic_msg.magnetic_field_covariance[0] =
+          imu_magnetic_msg.magnetic_field_covariance[4] =
+          imu_magnetic_msg.magnetic_field_covariance[8] = magnetic_field_cov;
+
+        ros::Time now = ros::Time::now();
+
+        imu_data_raw_msg.header.stamp =
+          imu_magnetic_msg.header.stamp = now;
+
+        imu_data_raw_msg.header.frame_id =
+          imu_magnetic_msg.header.frame_id = frame_id_;
+
+        // original data used the g unit, convert to m/s^2
+        imu_data_raw_msg.linear_acceleration.x = imu.ax * consts.CONVERTOR_G2A;
+        imu_data_raw_msg.linear_acceleration.y = imu.ay * consts.CONVERTOR_G2A;
+        imu_data_raw_msg.linear_acceleration.z = imu.az * consts.CONVERTOR_G2A;
+
+        // original data used the degree/s unit, convert to radian/s
+        imu_data_raw_msg.angular_velocity.x = imu.gx * consts.CONVERTOR_D2R;
+        imu_data_raw_msg.angular_velocity.y = imu.gy * consts.CONVERTOR_D2R;
+        imu_data_raw_msg.angular_velocity.z = imu.gz * consts.CONVERTOR_D2R;
+
+        // original data used the uTesla unit, convert to Tesla
+        imu_magnetic_msg.magnetic_field.x =  imu.mx / consts.CONVERTOR_UT2T;
+        imu_magnetic_msg.magnetic_field.y = imu.my / consts.CONVERTOR_UT2T;
+        imu_magnetic_msg.magnetic_field.z = imu.mz / consts.CONVERTOR_UT2T;
+
+        // original data used the celsius unit
+        imu_temperature_msg.data = imu.temperature;
+
+        // publish the IMU data
+        imu_data_raw_pub_.publish(imu_data_raw_msg);
+        imu_mag_pub_.publish(imu_magnetic_msg);
+        imu_temperature_pub_.publish(imu_temperature_msg);
+      }
+
+      return true;
+    }
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "rt_usb_9axisimu_driver");
+
+  std::string port = std::string("/dev/ttyACM0");
+
+  ros::param::get("~port", port);
+
+  RtUsb9axisimuDriverForROS sensor(port);
+
+  if(sensor.Start() == false)
+    ROS_ERROR("Start() returns false, please check your devices.\n");
+  else
+  {
+    ROS_INFO("RT imu driver initialization OK.\n");
+    if(sensor.GetAndPublishData() == false)
+      ROS_ERROR("GetAndPublishData() returns false, please check your devices.\n");
+  }
+  
+  ROS_INFO("Shutting down RT imu driver complete.\n");
+
+  return 0;
+}
